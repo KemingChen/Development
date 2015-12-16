01c5337956be9db440b07588c35e3fa4bb8cf634
KemingChen <believe75467@gmail.com>
2015-12-16 13:56:48 +0800

    add dir test1 example

--- app/app.js
+++ app/app.js
@@ -4,6 +4,12 @@ angular.module('app', [
     'ui.router'
 
 ]).config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/test2");
-    $stateProvider;
+    $urlRouterProvider.otherwise("/test2"); // 要改目前的預設 url
+    $stateProvider
+    // 加入起 (改 state 名稱 & url 位置 & html 檔案位置)
+        .state('test1', {
+            url: "/test1",
+            templateUrl: "dir-test1/html-t1.html"
+        })
+        // 加入止
 });
